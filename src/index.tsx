f3af45c8a4286208417d7e8234fb4af44c40abd2
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-07-17 16:41:00 +0300

    Migrate to R18 createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import "react-app-polyfill/ie9"; // For IE 9-11 support
 import "react-app-polyfill/ie11"; // For IE 11 support
 import "./polyfill";
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
