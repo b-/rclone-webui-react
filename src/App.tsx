6a7e0c9f0cfdaa014bcf81a62a8a30b0b50dacb1
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-07-17 17:46:48 +0300

    Move routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ErrorBoundary from "./ErrorHandling/ErrorBoundary";
+import routes from "./routes";
 
 const loading = () => (
   <div className="animated fadeIn pt-3 text-center">Loading...</div>
@@ -14,7 +15,7 @@ const loading = () => (
 const DefaultLayout = React.lazy(() => import("./containers/DefaultLayout"));
 
 // Pages
-const Login = React.lazy(() => import("./views/Pages/Login"));
+const Login = React.lazy(() => import("./views/Pages/Login/Login"));
 const Register = React.lazy(() => import("./views/Pages/Register"));
 const Page404 = React.lazy(() => import("./views/Pages/Page404"));
 const Page500 = React.lazy(() => import("./views/Pages/Page500"));
@@ -31,7 +32,18 @@ function App() {
               <Route path="/register" element={<Register />} />
               <Route path="/404" element={<Page404 />} />
               <Route path="/500" element={<Page500 />} />
-              <Route path="/" element={<DefaultLayout />} />
+              <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<DefaultLayout />} />
+                {routes.map((route, idx) => {
+                  return route.component ? (
+                    <Route
+                      key={idx}
+                      path={route.path}
+                      element={<route.component />}
+                    />
+                  ) : null;
+                })}
+              </Route>
             </Routes>
           </React.Suspense>
         </HashRouter>
---
+++ src/views/Pages/Login/Login.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import tw from "tailwind-styled-components";
+import { useNavigate } from "react-router-dom";
+import { UncontrolledAlert } from "reactstrap";
+import { connect, useDispatch } from "react-redux";
+import {
+  changeAuthKey,
+  changeIPAddress,
+  changeUserNamePassword,
+  signOut,
+} from "../../../actions/userActions";
+import axiosInstance from "../../../utils/API/API";
+import urls from "../../../utils/API/endpoint";
+import logo from "../../../assets/img/brand/logo_symbol.png";
+import { LOGIN_TOKEN } from "../../../utils/Constants";
+
+const Label = tw.label`block font-bold`;
+const Input = tw.input`py-1 px-2 border w-full`;
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [hostname, setHostname] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setHostname(window.location.href.split("#/")[0]);
+
+    if (hostname?.indexOf("?") !== -1) {
+      setHostname(window.location.href.split("?")[0]);
+    }
+  }, []);
+
+  useEffect(() => {
+    signOut();
+
+    let url = new URL(window.location.href);
+    let loginToken = url.searchParams.get(LOGIN_TOKEN);
+    if (url.searchParams.get("ip_address")) {
+      setHostname(url.searchParams.get("ip_address") || "");
+    }
+
+    if (loginToken) {
+      Promise.all([changeAuthKey(loginToken), changeIPAddress(hostname)]);
+      redirectToDashboard();
+    }
+  }, []);
+
+  const handleUsername = (e) => setUsername(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleHostname = (e) => setHostname(e.target.value);
+  const redirectToDashboard = () => navigate("/dashboard");
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    if (e) e.preventDefault();
+
+    Promise.all([
+      dispatch(changeUserNamePassword(username, password)),
+      dispatch(changeIPAddress(hostname)),
+    ]).then(() => {
+      axiosInstance.post(urls.noopAuth).then(
+        (data) => {
+          console.log("Connection successful.");
+          redirectToDashboard();
+        },
+        (error) => {
+          console.log(error);
+          setError(
+            "Error connecting. Please check your username password and make sure rclone is working properly at the specified IP."
+          );
+        }
+      );
+    });
+  };
+
+  return (
+    <div className="flex" data-test="loginComponent">
+      <div className="mx-auto">
+        <div className="flex justify-center pt-20 mb-10">
+          <img src={logo} alt="rclone" />
+        </div>
+
+        <form onSubmit={onSubmit} className="border w-96 p-10">
+          {error && (
+            <div
+              className="bg-red-100 text-red-600 p-3 mb-3"
+              children={error}
+            />
+          )}
+          <div className="mb-3">
+            <Label htmlFor="host">Host</Label>
+            <Input
+              id="host"
+              type="text"
+              autoComplete="url"
+              onChange={handleHostname}
+              defaultValue={hostname}
+              data-testid="LoginForm-ipAddress"
+            />
+          </div>
+          <div className="mb-3">
+            <Label htmlFor="host">Username</Label>
+            <Input
+              type="text"
+              autoComplete="username"
+              data-testid="LoginForm-userName"
+              onChange={handleUsername}
+              value={username}
+            />
+          </div>
+          <div className="mb-3">
+            <Label htmlFor="host">Password</Label>
+            <Input
+              type="password"
+              placeholder="Password"
+              data-testid="LoginForm-password"
+              autoComplete="current-password"
+              onChange={handlePassword}
+              value={password}
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              className="px-4 py-2 bg-black text-white font-bold uppercase"
+              type="submit"
+              data-testid="LoginForm-BtnLogin"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default connect(null, {})(Login);
