e4850c761e64e0ceeb0cb4bc7fc28972725f2c25
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-07-20 19:45:07 +0300

    Make BandwidthStatusCard tailwind

--- src/views/Base/BandwidthStatusCard/BandwidthStatusCard.js
+++ src/views/Base/BandwidthStatusCard/BandwidthStatusCard.js
@@ -2,57 +2,32 @@ import React from "react";
 import { validateSizeSuffix } from "../../../utils/Tools";
 import { toast } from "react-toastify";
 import {
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Form,
-  FormFeedback,
-  FormGroup,
-  Input,
-  Label,
-} from "reactstrap";
-import { connect } from "react-redux";
-import { getBandwidth, setBandwidth } from "../../../actions/statusActions";
-import * as PropTypes from "prop-types";
-import { PROP_BANDWIDTH } from "../../../utils/RclonePropTypes";
+  getCurrentBandwidthSetting,
+  setCurrentBandwidthSetting,
+} from "rclone-api";
 
-class BandwidthStatusCard extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      bandwidthText: "",
-      hasError: false,
-      showChangeBandwidth: false,
-    };
-  }
-
-  /**
-   * Get the current bandwidth from the backend
-   */
-  getBandwidth = () => {
-    const { getBandwidth } = this.props;
-    getBandwidth();
-  };
+function BandwidthStatusCard() {
+  const [bandwidth, setBandwidth] = React.useState();
+  const [bandwidthText, setBandwidthText] = React.useState("");
+  const [hasError, setHasError] = React.useState(false);
 
   /**
    * Set the new bandwidth specified in state.bandwidthText
    * Check if text is valid, before sending.
    */
-  setBandwidth = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { bandwidthText, hasError } = this.state;
-    if (!hasError) {
-      const { setBandwidth } = this.props;
-      if (bandwidthText) {
-        setBandwidth(bandwidthText);
-        this.toggleShowChangeBandwidth();
-      } else {
-        setBandwidth("0M");
-      }
-    } else {
+    if (hasError) {
       toast.error("Please check the errors before submitting");
+      return false;
+    }
+
+    if (bandwidthText) {
+      setCurrentBandwidthSetting(bandwidthText).then((res) =>
+        setBandwidth(res)
+      );
+    } else {
+      setCurrentBandwidthSetting("0M").then((res) => setBandwidth(res));
     }
   };
 
@@ -61,12 +36,12 @@ class BandwidthStatusCard extends React.Component {
    * Validate input before setting, if the input text is invalid, set the hasError in the state.
    * @param e
    */
-  changeBandwidthInput = (e) => {
+  const changeBandwidthInput = (e) => {
     const inputValue = e.target.value;
     let validateInput = false;
     if (inputValue === "") {
       validateInput = true;
-    } else if (inputValue) {
+    } else {
       const splitValue = inputValue.split(":");
       if (splitValue.length === 1) {
         validateInput = validateSizeSuffix(splitValue[0]);
@@ -76,111 +51,47 @@ class BandwidthStatusCard extends React.Component {
         validateInput = validateDownloadLimit && validateUploadLimit;
       }
     }
-    this.setState({
-      bandwidthText: inputValue,
-      hasError: !validateInput,
-    });
+    setBandwidthText(inputValue);
+    setHasError(!validateInput);
   };
 
-  /**
-   * Upon first shallow, get the current bandwidth
-   */
-  componentDidMount() {
-    this.getBandwidth();
-  }
+  React.useEffect(() => {
+    getCurrentBandwidthSetting().then((res) => setBandwidth(res));
+  }, []);
 
-  /**
-   * Show or hide the right side modal with the form to change the current bandwidth.
-   */
-  toggleShowChangeBandwidth = () => {
-    this.setState((prevState) => ({
-      showChangeBandwidth: !prevState.showChangeBandwidth,
-    }));
-  };
-
-  render() {
-    const { bandwidthText, hasError, showChangeBandwidth } = this.state;
-    const { bandwidth } = this.props;
-
-    return (
-      <Card>
-        <CardHeader>
-          Bandwidth{" "}
-          <button
-            className="btn btn-white float-right"
-            onClick={this.toggleShowChangeBandwidth}
-          >
-            Modify
+  return (
+    <div className="border mb-2">
+      <div className="bg-gray-200 px-2 py-1">Bandwidth</div>
+      <div className="px-2 py-2">
+        <p>Current Max speed: {bandwidth?.rate}</p>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="bandwidthValue">
+              Enter new max speed (upload:download)
+            </label>
+            <div className="mb-3">
+              <input
+                className="border"
+                type="text"
+                value={bandwidthText}
+                valid={!hasError}
+                invalid={hasError}
+                id="bandwidthValue"
+                onChange={changeBandwidthInput}
+              />
+              <p className="text-gray-600">
+                The bandwidth should be of the form 1M|2M|1G|1K|1.1K etc. Can
+                also be specified as (upload:download). Make empty to reset.
+              </p>
+            </div>
+          </div>
+          <button type="submit" className="bg-black px-4 text-white">
+            Set
           </button>
-        </CardHeader>
-        <CardBody>
-          <p>
-            <span className="card-subtitle">Current Max speed: {"  "}</span>
-            <span className="card-text">
-              {bandwidth.rate !== "off" ? bandwidth.rate : "Unlimited"}
-            </span>
-          </p>
-          <Form
-            onSubmit={this.setBandwidth}
-            className={showChangeBandwidth ? "" : "d-none"}
-          >
-            <FormGroup row>
-              <Label for="bandwidthValue" sm={5}>
-                Enter new max speed (upload:download)
-              </Label>
-              <Col sm={7}>
-                <Input
-                  type="text"
-                  value={bandwidthText}
-                  valid={!hasError}
-                  invalid={hasError}
-                  id="bandwidthValue"
-                  onChange={this.changeBandwidthInput}
-                ></Input>
-                <FormFeedback valid>Keep empty to reset.</FormFeedback>
-                <FormFeedback>
-                  The bandwidth should be of the form 1M|2M|1G|1K|1.1K etc. Can
-                  also be specified as (upload:download)
-                </FormFeedback>
-              </Col>
-            </FormGroup>
-            <Button className="float-right" color="success" type="submit">
-              Set
-            </Button>
-          </Form>
-        </CardBody>
-      </Card>
-    );
-  }
+        </form>
+      </div>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  /**
-   * Connectivity status with backend
-   */
-  isConnected: state.status.isConnected,
-  /**
-   * Map with {bytesPerSecond, rate}
-   */
-  bandwidth: state.status.bandwidth,
-});
-
-BandwidthStatusCard.propTypes = {
-  isConnected: PropTypes.bool.isRequired,
-  /**
-   * Determines currently set bandwidth
-   */
-  bandwidth: PROP_BANDWIDTH,
-  /**
-   * Redux function to get the current bandwidth.
-   */
-  getBandwidth: PropTypes.func.isRequired,
-  /**
-   * Redux function to set the new bandwidth in rclone backend.
-   */
-  setBandwidth: PropTypes.func.isRequired,
-};
-
-export default connect(mapStateToProps, { getBandwidth, setBandwidth })(
-  BandwidthStatusCard
-);
+export default BandwidthStatusCard;
