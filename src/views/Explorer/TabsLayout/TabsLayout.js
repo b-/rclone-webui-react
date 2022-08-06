c6626fe68d5cb8f7af03932d2568bcb713fb6040
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-08-07 01:02:58 +0300

    Drop reactstrap from tabs

--- src/views/Explorer/TabsLayout/TabsLayout.js
+++ src/views/Explorer/TabsLayout/TabsLayout.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button, Nav, NavItem, NavLink } from "reactstrap";
 import { connect } from "react-redux";
 import {
   addRemoteContainer,
   changeActiveRemoteContainer,
   removeRemoteContainer,
 } from "../../../actions/explorerActions";
+import cn from "classnames";
 
 function TabsLayout(props) {
   const {
@@ -19,51 +19,58 @@ function TabsLayout(props) {
     ? activeRemoteContainerID[currentPaneID]
     : "";
   return (
-    <Nav tabs className="d-flex">
-      {containers.map(({ ID, paneID }) => {
-        if (currentPaneID === paneID) {
-          const isActiveTab = ID === activeContainerIDInPane;
-          return (
-            <NavItem
-              key={"TAB_" + ID}
-              className={
-                "pl-0 pr-0 custom-tab " + (isActiveTab ? "tab-active" : "")
-              }
-            >
-              <NavLink
-                className={"float-center"}
-                onClick={() => changeActiveRemoteContainer(ID, paneID)}
+    <nav>
+      <ul className="flex">
+        {containers.map(({ ID, paneID }) => {
+          if (currentPaneID === paneID) {
+            const isActiveTab = ID === activeContainerIDInPane;
+            return (
+              <li
+                key={"TAB_" + ID}
+                className={cn("")}
+                onAuxClick={() => removeRemoteContainer(ID, paneID)}
               >
-                Folder
-                <span className="overflow-hidden">
+                <button
+                  className={cn(
+                    "transition overflow-hidden p-2 rounded-tl-lg",
+                    {
+                      "bg-gray-200": isActiveTab,
+                    }
+                  )}
+                  type="button"
+                  onClick={() => changeActiveRemoteContainer(ID, paneID)}
+                >
                   {currentPaths[ID] && currentPaths[ID].remoteName !== ""
                     ? currentPaths[ID].remoteName
-                    : "Open New"}
-                </span>
-                <Button
-                  className="btn-no-background btn btn-secondary p-0 pl-1 pr-2 ml-2"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    removeRemoteContainer(ID, paneID);
-                  }}
+                    : "New Tab"}
+                </button>
+                <button
+                  className={cn(
+                    "transition overflow-hidden p-2 rounded-tr-lg",
+                    {
+                      "bg-gray-200": isActiveTab,
+                    }
+                  )}
+                  type="button"
+                  onClick={() => removeRemoteContainer(ID, paneID)}
                 >
-                  Close
-                </Button>
-              </NavLink>
-            </NavItem>
-          );
-        }
-        return null;
-      })}
-      <NavItem key={"ADD_BUTTON"} className={"pl-0 pr-0"}>
-        <NavLink
-          className={"float-center"}
-          onClick={() => addRemoteContainer(currentPaneID)}
-        >
-          plus
-        </NavLink>
-      </NavItem>
-    </Nav>
+                  &times; <span className="sr-only">Close tab</span>
+                </button>
+              </li>
+            );
+          }
+          return null;
+        })}
+        <li key={"ADD_BUTTON"} className={"p-2"}>
+          <button
+            type="button"
+            onClick={() => addRemoteContainer(currentPaneID)}
+          >
+            New Tab
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
