e1852e0578a0037b2d805fd4a8700abbfe2581fa
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-08-07 01:34:00 +0300

    Transform to function component

--- src/views/Explorer/RemotesList/RemoteListAutoSuggest.js
+++ src/views/Explorer/RemotesList/RemoteListAutoSuggest.js
@@ -1,75 +1,39 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = (config, value) => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
+function RemoteListAutoSuggest({ value, onChange }) {
+  const [suggestions, setSuggestions] = React.useState([]);
 
-  if (inputLength === 0) {
-    return config;
-  }
+  React.useEffect(
+    () =>
+      setSuggestions({
+        suggestions: this.props.suggestions,
+      }),
+    []
+  );
 
-  return inputLength === 0
-    ? []
-    : config.filter(
-        (lang) => lang.toLowerCase().slice(0, inputLength) === inputValue
-      );
-};
+  return (
+    <>
+      <label htmlFor="remote" className="sr-only">
+        Remote
+      </label>
+      <input
+        list="datalist-remote"
+        id="remote"
+        className="w-full"
+        defaultValue={value}
+        onChange={onChange}
+      />
 
-class RemoteListAutoSuggest extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
-    this.state = {
-      suggestions: [],
-    };
-  }
-
-  componentDidMount = () => {
-    this.setState({
-      suggestions: getSuggestions(this.props.suggestions, ""),
-    });
-  };
-
-  // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
-  };
-
-  render() {
-    const { value, onChange } = this.props;
-
-    return (
-      <>
-        <label htmlFor="remote" className="sr-only">
-          Remote
-        </label>
-        <input
-          list="datalist-remote"
-          id="remote"
-          className="w-full"
-          defaultValue={value}
-          onChange={onChange}
-        />
-
-        <datalist id="datalist-remote">
-          {this.state.suggestions.map((suggestion) => (
-            <option key={suggestion} value={suggestion}>
-              {suggestion}
-            </option>
-          ))}
-        </datalist>
-      </>
-    );
-  }
+      <datalist id="datalist-remote">
+        {this.state.suggestions.map((suggestion) => (
+          <option key={suggestion} value={suggestion}>
+            {suggestion}
+          </option>
+        ))}
+      </datalist>
+    </>
+  );
 }
 
 RemoteListAutoSuggest.propTypes = {
