42f9becbf67072e08f0343be6fc3e05b25733812
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-08-19 19:20:02 +0300

    Clean Login component

--- src/views/Pages/Login.tsx
+++ src/views/Pages/Login.tsx
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from "react";
 import tw from "tailwind-styled-components";
 import { useNavigate } from "react-router-dom";
-import { connect, useDispatch } from "react-redux";
-import {
-  changeAuthKey,
-  changeIPAddress,
-  changeUserNamePassword,
-  signOut,
-} from "../../actions/userActions";
+import { signOut } from "../../actions/userActions";
 import axiosInstance from "../../utils/API/API";
 import urls from "../../utils/API/endpoint";
 import logo from "../../assets/img/brand/logo.svg";
-import { LOGIN_TOKEN } from "../../utils/Constants";
 
 const Label = tw.label`block font-bold`;
 const Input = tw.input`py-1 px-2 border w-full`;
@@ -24,62 +17,43 @@ function Login() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  //@todo: what does this do
-  useEffect(() => {
-    setHostname(window.location.href.split("#/")[0]);
-
-    if (hostname?.indexOf("?") !== -1) {
-      setHostname(window.location.href.split("?")[0]);
-    }
-  }, []);
-
   useEffect(() => {
     signOut();
-
-    let url = new URL(window.location.href);
-    let loginToken = url.searchParams.get(LOGIN_TOKEN);
-    if (url.searchParams.get("ip_address")) {
-      setHostname(url.searchParams.get("ip_address") || "");
-    }
-
-    if (loginToken) {
-      Promise.all([changeAuthKey(loginToken), changeIPAddress(hostname)]);
-      redirectToDashboard();
-    }
+    const host = new URL(window.location.href.split("#/")[0]);
+    setHostname(host.origin + "/" + (host.searchParams.get("ip") || ""));
   }, []);
 
   const handleUsername = (e) => setUsername(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
   const handleHostname = (e) => setHostname(e.target.value);
-  const redirectToDashboard = () => navigate("/");
-
-  const dispatch = useDispatch();
 
   const onSubmit = (e) => {
     if (e) e.preventDefault();
 
-    Promise.all([
-      dispatch(changeUserNamePassword(username, password)),
-      dispatch(changeIPAddress(hostname)),
-    ]).then(() => {
-      axiosInstance.post(urls.noopAuth).then(
-        (data) => {
-          console.log("Connection successful.");
-          redirectToDashboard();
-        },
-        (error) => {
-          console.log(error);
-          setErrorCode(error);
-          setError(
-            "Error connecting. Please check your username password and make sure rclone is working properly at the specified IP."
-          );
-        }
-      );
-    });
+    // This gives false sense of security. In a perfect world
+    // a backend would have set the key as a cookie with both
+    // the Secure & Http-only flags enabled
+    const auth = window.btoa(`${username}:${password}`);
+    window.localStorage.setItem("authKey", auth);
+    window.localStorage.setItem("ipAddress", hostname);
+
+    axiosInstance
+      .post(urls.noopAuth)
+      .then((data) => {
+        console.log("Connection successful.");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorCode(error);
+        setError(
+          "Error connecting. Please check your username password and make sure rclone is working properly at the specified IP."
+        );
+      });
   };
 
   return (
-    <div className="flex" data-test="loginComponent">
+    <div className="flex">
       <div className="mx-auto">
         <div className="flex justify-center pt-20 mb-10">
           <img src={logo} alt="rclone" className="w-40" />
@@ -101,10 +75,9 @@ function Login() {
             <Input
               id="host"
               type="text"
-              autoComplete="url"
+              autoComplete=""
               onChange={handleHostname}
-              defaultValue={hostname}
-              data-testid="LoginForm-ipAddress"
+              value={hostname}
             />
           </div>
           <div className="mb-3">
@@ -112,7 +85,6 @@ function Login() {
             <Input
               type="text"
               autoComplete="username"
-              data-testid="LoginForm-userName"
               onChange={handleUsername}
               value={username}
             />
@@ -121,8 +93,6 @@ function Login() {
             <Label htmlFor="host">Password</Label>
             <Input
               type="password"
-              placeholder="Password"
-              data-testid="LoginForm-password"
               autoComplete="current-password"
               onChange={handlePassword}
               value={password}
@@ -132,7 +102,6 @@ function Login() {
             <button
               className="px-4 py-2 bg-black text-white font-bold uppercase"
               type="submit"
-              data-testid="LoginForm-BtnLogin"
             >
               Login
             </button>
@@ -143,4 +112,4 @@ function Login() {
   );
 }
 
-export default connect(null, {})(Login);
+export default Login;
